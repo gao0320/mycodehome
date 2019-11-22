d8158dffed14b187ece187271c6c2a2f0ffa439d
GaoJ <gao@666.com>
2019-11-22 17:53:59 +0800

    提炼用户模块

--- web_back/utils/user.js
+++ web_back/utils/user.js
@@ -1,38 +1,24 @@
 
-var baseUrl='http://localhost:8000/'
+var baseUrl = 'http://localhost:8000/'
 var user = {
-    logout:function(){
-        $.post(baseUrl+'admin/logout',function(res){
-        console.log(res);
-        if(res.code===200){
-            window.location.href='./login.html'
-        }                
-    })
+    logout: function (callback) {
+        $.post(baseUrl + 'admin/logout', function (res) {
+            callback(res)
+        })
+    }
+    ,
+    getInfo: function (callback) {
+        $.get(baseUrl + 'admin/getuser', function (res) {
+            callback(res)
+        })
+    }
+    ,
+    login: function (myName, myPassword,callback) {
+        // $.post(地址，参数，回调)
+        $.post(baseUrl + 'admin/login',
+            { user_name: myName, password: myPassword },
+            function (res) {
+                callback(res)
+            })
+    }
 }
-,
-   getIfo:function(){
-       $.get(baseUrl+'admin/getuser',function(res){
-        if(res.code===200){
-            $('#userImg').prop('src',res.data.user_pic)
-            $('#userName').text(res.data.nickname)
-        }
-   })
-   }
-   ,
-       login: function(myName,myPassword){
-                    // $.post(地址，参数，回调)
-       $.post(baseUrl+'admin/login',
-         {user_name:myName,password:myPassword},
-         function(res){
-            console.log(res);
-         if(res.code===200){
-            
-            location.href='./index.html'
-         }else{
-            $('#myModal').modal('show')
-            $('#msgInfo').text(res.msg)
-         }
-        
-    })
-       }
-   }
